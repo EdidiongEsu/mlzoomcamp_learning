7e437f47b2e4b8894c341f45da69a23b60b13b38
Edidiong Esu <eddy.esu.22@gmail.com>
2022-10-23 20:24:44 +0100

    Week 7.5

--- WEEK 7/service.py
+++ WEEK 7/service.py
@@ -1,6 +1,31 @@
+from email.mime import application
 from unittest import result
+
 import bentoml
 from bentoml.io import JSON
+from bentoml.io import NumpyNdarray
+
+import pydantic
+from pydantic import BaseModel
+
+import numpy as np
+
+
+class CreditApplication(BaseModel):
+    seniority: int
+    home: str
+    time: int
+    age: int
+    marital:  str
+    records: str
+    job: str
+    expenses: int
+    income: float
+    assets: float
+    debt: float
+    amount: int
+    price: int
+
 
 model_ref = bentoml.xgboost.get("credit_risk_model:latest")
 # credit_risk_model:iusktlssmgtxfshc -- latest chooses teh most recent tag
@@ -13,8 +38,9 @@ svc = bentoml.Service(
 )
 
 
-@svc.api(input=JSON(), output=JSON())
-def classify(application_data):
+@svc.api(input= NumpyNdarray(shape=(-1,29), dtype=np.float32,enforce_dtype=True,enforce_shape = True), output=JSON())
+def classify(vector):
+    application_data = credit_application.dict()
     vector = dv.transform(application_data)
     prediction = model_runner.predict.run(vector)
     print(prediction)
