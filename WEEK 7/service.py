97a97cdfe4638b8aae9a2df79afef7fa68e9745c
Edidiong Esu <eddy.esu.22@gmail.com>
2022-10-25 16:29:03 +0100

    Week 7

--- WEEK 7/service.py
+++ WEEK 7/service.py
@@ -1,55 +1,32 @@
-from email.mime import application
-from unittest import result
+import numpy as np
 
 import bentoml
 from bentoml.io import JSON
-from bentoml.io import NumpyNdarray
-
-import pydantic
-from pydantic import BaseModel
-
-import numpy as np
-
-
-class CreditApplication(BaseModel):
-    seniority: int
-    home: str
-    time: int
-    age: int
-    marital:  str
-    records: str
-    job: str
-    expenses: int
-    income: float
-    assets: float
-    debt: float
-    amount: int
-    price: int
-
 
 model_ref = bentoml.xgboost.get("credit_risk_model:latest")
-# credit_risk_model:iusktlssmgtxfshc -- latest chooses teh most recent tag
 dv = model_ref.custom_objects['dictVectorizer']
 
 model_runner = model_ref.to_runner()
 
-svc = bentoml.Service(
-    'credit_risk_classifier', runners=[model_runner]
-)
+svc = bentoml.Service("credit_risk_classifier", runners=[model_runner])
 
 
-@svc.api(input= NumpyNdarray(shape=(-1,29), dtype=np.float32,enforce_dtype=True,enforce_shape = True), output=JSON())
-def classify(vector):
-    application_data = credit_application.dict()
+@svc.api(input=JSON(), output=JSON())
+async def classify(application_data):
     vector = dv.transform(application_data)
-    prediction = model_runner.predict.run(vector)
+    prediction = await model_runner.predict.async_run(vector)
     print(prediction)
-
     result = prediction[0]
 
     if result > 0.5:
-        return {"status": "Declined"}
-    elif result > 0.2:
-        return {"status": 'Maybe'}
+        return {
+            "status": "DECLINED"
+        }
+    elif result > 0.25:
+        return {
+            "status": "MAYBE"
+        }
     else:
-        return {"status": 'Approved'}
+        return {
+            "status": "APPROVED"
+        }
