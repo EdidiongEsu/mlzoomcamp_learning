883ea527bcbb9f547a94274ed23b1f7276b8c63c
Edidiong Esu <eddy.esu.22@gmail.com>
2022-11-10 10:46:19 +0100

    update locust file

--- projects/midterm_project/locustfile.py
+++ projects/midterm_project/locustfile.py
@@ -3,20 +3,27 @@ from locust import task
 from locust import between
 from locust import HttpUser
 
-sample = {"seniority": 3,
-          "home": "owner",
-          "time": 36,
-          "age": 26,
-          "marital": "single",
-          "records": "no",
-          "job": "freelance",
-          "expenses": 35,
-          "income": 0.0,
-          "assets": 60000.0,
-          "debt": 3000.0,
-          "amount": 800,
-          "price": 1000
-          }
+sample = {
+    "age": 12,
+    "gender": "m",
+    "region_category": "missing",
+    "membership_category": "silver_membership",
+    "joined_through_referral": "no",
+    "preferred_offer_types": "credit/debit_card_offers",
+    "medium_of_operation": "smartphone",
+    "internet_option": "fiber_optic",
+    "last_visit_time": "13:38:19",
+    "days_since_last_login": 24.0,
+    "avg_time_spent": 121.82,
+    "avg_transaction_value": 30385.88,
+    "avg_frequency_login_days": 25.0,
+    "points_in_wallet": 698.62,
+    "used_special_discount": "no",
+    "offer_application_preference": "yes",
+    "past_complaint": "yes",
+    "complaint_status": "solved",
+    "feedback": "too_many_ads"
+}
 
 
 class CreditRiskTestUser(HttpUser):
