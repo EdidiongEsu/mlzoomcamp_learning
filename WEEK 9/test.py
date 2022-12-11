9d1eda79e55bc342a444bdf20fb980da0af4bbe5
Edidiong Esu <eddy.esu.22@gmail.com>
2022-12-11 14:50:16 +0100

    update files

--- WEEK 9/test.py
+++ WEEK 9/test.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+##url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url = 'https://e5y03mb55i.execute-api.us-east-1.amazonaws.com/test/predict'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
